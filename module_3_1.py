f176657643c182d7ef205b38ddbd163c5957c1eb
M1stakesWereMade <superlexlive@gmail.com>
2024-07-23 15:14:07 +0300

    module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -1,19 +1,14 @@
 calls = 0
 
 def count_calls():
-    """Функция для подсчета вызовов других функций."""
     global calls
     calls += 1
 
 def string_info(string):
-    """Функция, принимающая строку и возвращающая кортеж из длины строки,
-    строки в верхнем регистре и строки в нижнем регистре."""
     count_calls()
     return len(string), string.upper(), string.lower()
 
 def is_contains(string, list_to_search):
-    """Функция, принимающая строку и список, и возвращающая True, если строка
-    присутствует в списке, игнорируя регистр."""
     count_calls()
     normalized_string = string.lower()
     for item in list_to_search:
